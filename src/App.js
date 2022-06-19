1e122af1e8fc52fb981a1f8115c310b64c7b4df2
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-06-19 19:26:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Talifhani</h1>
+
+        <ul>
+          <li>Home</li>
+          <li>About me</li>
+          <li>Projects</li>
+          <li>Skills</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
