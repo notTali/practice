dc3909a4999ee8d12cc62287cf4c1f34a1f1a9ce
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-06-19 19:51:55 +0200

    Nav Bar Component

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return ( 
+        <nav className="nav-bar">
+            <h1>notTali Blogs</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default NavBar;
