e478fd724f394fc71975624f1f7215102a197770
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-06-19 22:43:20 +0200

    events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
 const Home = () => {
+    const handleClick = function(e, name){
+        console.log(name + ' says hello!', e.target); 
+    }
+
     return ( 
         <div className="home">
             <h1>Home</h1>
+            <button onClick={(e)=>{handleClick(e, "Dove")}}>Click</button>
         </div>
      );
 }
