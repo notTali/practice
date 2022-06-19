af666d920f8e974005379daaefdcfcf75bc671cc
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-06-20 00:09:25 +0200

    Usestate Hook

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,21 @@
+import { useState } from "react";
 const Home = () => {
-    const handleClick = function(e, name){
-        console.log(name + ' says hello!', e.target); 
-    }
-
+    const [blogs, setBlogs] = useState([
+        {title: "Django website", body:'Lorem ipsum dolor sit amet.', author:"Tali", id: 1},
+        {title: "React website", body:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aliquam adipisci eum qui asperiores magnam ad nihil rerum hic laborum.', author:"Lover", id: 2},
+        {title: "Django & React", body:'Lorem ipsum dolor sit amet.', author:"Rili", id: 3},
+        {title: "HTML & CSS website", body:'Lorem ipsum dolor sit amet.', author:"Linde", id: 4},
+    ]);
+    
     return ( 
         <div className="home">
-            <h1>Home</h1>
-            <button onClick={(e)=>{handleClick(e, "Dove")}}>Click</button>
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by <span>{blog.author}</span></p>
+                    <p style={{color: "blue"}}>{blog.body}</p>
+                </div>
+            ))}
         </div>
      );
 }
