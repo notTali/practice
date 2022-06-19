9216448e288026344032084f47aed149d9e8266c
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-06-19 20:35:43 +0200

    CSS styling

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,7 @@
 const NavBar = () => {
     return ( 
         <nav className="nav-bar">
-            <h1>notTali Blogs</h1>
+            <h1>not<span>Tali</span> Blogs</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">New Blog</a>
